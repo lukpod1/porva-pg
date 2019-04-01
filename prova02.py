05bf47ebd3bddedb8f2aecbad5f447b52bd9cc86
José Lucas <lukasalvespod1@gmail.com>
2019-04-01 16:28:34 -0300

    modif

--- prova02.py
+++ prova02.py
@@ -1,33 +1,9 @@
 import matplotlib.pyplot as plt
 from matplotlib.path import Path
 import matplotlib.patches as pactches
-
-vertices = [
-    (-3,6),
-    (3,6),
-    (6,0),
-    (3,-6),
-    (-3,-6),
-    (-6,0),
-    (0.,0.)
-    
-]
-comandos = [
-    Path.MOVETO,
-    Path.LINETO,
-    Path.LINETO,
-    Path.LINETO,
-    Path.LINETO,
-    Path.LINETO,
-    Path.CLOSEPOLY
-]
-
-path = Path(vertices, comandos)
-patch = pactches.PathPatch(path, facecolor='blue', lw=2)
-fig = plt.figure()
-ax = fig.add_subplot(111)
-ax.add_patch(patch)
-ax.set_xlim(-10, 10)
-ax.set_ylim(-10, 10)
+x = [-3,3,6,3,-3,-6,-3]
+y = [6,6,0,-6,-6,0,6]
+plt.plot( x, y, 'go') # green bolinha
+plt.plot( x, y, 'k:', color='orange') # linha pontilha orange
 plt.grid(True)
 plt.show()
